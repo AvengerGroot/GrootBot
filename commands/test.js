5e836fa6c84a659ad3a8e4f2991c91c193b68dac
Avenger <32858010+AvengerGroot@users.noreply.github.com>
2017-12-13 06:37:11 -0800

    Delete test.js

--- commands/test.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-exports.run = (client, message, args) => {
-  const embed = new Discord.RichEmbed()
-		.setDescription(`**${message.guild.name}** Icon`)
-    .setImage(`${message.guild.iconURL}`)
-		message.channel.send({embed})
-}
