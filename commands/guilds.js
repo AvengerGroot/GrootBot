20f7d64d1692119e945bd5ce2b78e6b31d4c9af3
Avenger <32858010+AvengerGroot@users.noreply.github.com>
2018-01-27 07:22:27 +0200

    Update guilds.js

--- commands/guilds.js
+++ commands/guilds.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
   .setDescription(`I am on **${message.client.guilds.size}** servers! :house_with_garden: `)
-  .setColor("#B15E3A")
   .setTimestamp();
   message.channel.send({ embed });
 }
