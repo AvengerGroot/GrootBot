be61a931a1bedd4725c28aef422c152578062850
Avenger <32858010+AvengerGroot@users.noreply.github.com>
2018-01-27 07:27:18 +0200

    Update icon.js

--- commands/icon.js
+++ commands/icon.js
@@ -1,7 +1,12 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+  let output = message.guild.iconURL
+if(!output) {
+  return message.channel.send(`**${message.guild.name}** does not have an icon`)
+}
   const embed = new Discord.RichEmbed()
 		.setDescription(`**${message.guild.name}** Icon`)
     .setImage(`${message.guild.iconURL}`)
+
 		message.channel.send({embed})
 }
