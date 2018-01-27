1799ebbd8dd00bebeaad9650602b680d905b9290
Avenger <32858010+AvengerGroot@users.noreply.github.com>
2018-01-27 07:22:57 +0200

    Update icon.js

--- commands/icon.js
+++ commands/icon.js
@@ -3,6 +3,5 @@ exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
 		.setDescription(`**${message.guild.name}** Icon`)
     .setImage(`${message.guild.iconURL}`)
-		.setColor("#B15E3A")
 		message.channel.send({embed})
 }
