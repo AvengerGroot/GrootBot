313d6b9c3eb11109ae86ea571a23a08cc917b51e
Avenger <32858010+AvengerGroot@users.noreply.github.com>
2018-01-27 07:22:08 +0200

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -3,7 +3,6 @@ const embed = new Discord.RichEmbed();
 
 exports.run = function(client, message, args) {
   embed.setTitle("__Groot Source Code__")
-  .setColor("#9CDAF0")
     .setDescription("**Groot is open-source!\n The source code is over at https://github.com/AvengerGroot/GrootBot**")
     .setThumbnail("https://assets-cdn.github.com/images/modules/logos_page/Octocat.png")
   message.channel.send({embed});
