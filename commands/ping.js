6ea7cc48e6c138560bb9294fa81b9e956f1fc808
Avenger <32858010+AvengerGroot@users.noreply.github.com>
2018-03-30 06:10:08 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
-		.setDescription(`:ping_pong: Pong! Time took: ${Date.now() - message.createdTimestamp} ms`)
+               .setDescription(`:ping_pong: | Pong! Time taken: **${Date.now() - message.createdTimestamp} ms**`)
 		message.channel.send({embed})
 }
